cb82813937f7025f8cad3062cddc85c2eec570fc
bert-w <bertwijnhoven@hotmail.com>
2022-08-31 01:14:07 +0200

    add form functionality

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{html,ts}", "./projects/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [require("@tailwindcss/forms")],
-};
